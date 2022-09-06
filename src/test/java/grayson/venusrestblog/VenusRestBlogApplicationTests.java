63b83561c6a8c4a33fef745b0cbed94c95fa006b
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-09-06 15:05:27 -0500

    first commit

---
+++ src/test/java/grayson/venusrestblog/VenusRestBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package grayson.venusrestblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusRestBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
